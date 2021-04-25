87099d79ec13df240de15b151dd770b12c1318dd
StasBeep <stas-stas.stanislav@mail.ru>
2021-04-25 19:01:06 +0300

    Добавление дополнительных заданий

---
+++ lesson-2/additional-exercise/jun.js
@@ -0,0 +1,14 @@
+// -10 -> '(10)'
+// x >= 0 -> 'x'
+
+function toAccounting(n) {
+    if (n < 0) {
+        return '(' + Math.abs(n) + ')'
+    } else {
+        return n.toString();
+    }
+}
+console.log(toAccounting(undefined));
+console.log(toAccounting(0))
+console.log(toAccounting(10))
+console.log(toAccounting(-5))
---
+++ lesson-2/additional-exercise/reverse.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let reversedNumber = Math.abs(x).toString().split('').reverse().join('');
+
+    reversedNumber = x >= 0 ? +reversedNumber : -reversedNumber;
+    return reversedNumber <= -Math.pow(2, 31) || reversedNumber >= Math.pow(2, 31) -1
+        ? 0 : reversedNumber;
+};
+
+console.log(reverse(-10)); // -1
+console.log(reverse(0)); // 0
+console.log(reverse(12345)); // 54321
+console.log(reverse(-567)); // -765
+console.log(reverse(4500)); // 54
