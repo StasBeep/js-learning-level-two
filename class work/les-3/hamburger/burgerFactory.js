0de023633c399e864ac17233cbdc68e373affefd
StasBeep <stas-stas.stanislav@mail.ru>
2021-05-05 22:51:43 +0300

    Добавление всех файлов по 3 лекции

---
+++ class work/les-3/hamburger/burgerFactory.js
@@ -0,0 +1,46 @@
+class Param {
+    constructor(element) {
+        this.name = element.value;
+        this.price = +element.dataset['price'];
+        this.calories = +element.dataset['calories'];
+    }
+}
+
+class Burger {
+    constructor(size, add, topping){
+        this.size = new Param(this._select(size));
+        this.add = new Param(this._select(add));
+        this.toppings = this._getToppings(topping);
+    }
+
+    _select(name) {
+        return document.querySelector(`input[name="${name}"]:checked`);
+    }
+
+    _getToppings(name) {
+        let result = [];
+        this._selectAll(name).forEach(el => result.push(new Param(el)));
+        return result;
+    }
+
+    _selectAll(name) {
+        return [...document.querySelectorAll(`input[name="${name}"]:checked`)];
+    }
+
+    _sumPrice() {
+        let result = this.size.price + this.add.price;
+        this.toppings.forEach(topping => result += topping.price);
+        return result;
+    }
+
+    _sumCalories() {
+        let result = this.size.calories + this.add.calories;
+        this.toppings.forEach(topping => result += topping.calories);
+        return result;
+    }
+
+    showSum(price, calories){
+        document.querySelector(price).textContent = this._sumPrice();
+        document.querySelector(calories).textContent = this._sumCalories();
+    }
+}
